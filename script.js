740fcd4b1b64fd6bbe4f15123e275177c89f5d8c
LeonardoBruchez <bruchezleonardo@gmail.com>
2025-10-21 22:01:35 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function Load(){
+  var msg = document.querySelector('p#msg')
+  var img = document.querySelector('img#imagem')
+  var now = new Date()
+  var hora = now.getHours()
+  var minuto = now.getMinutes()
+  msg.textContent = `Agora são ${hora} horas e ${minuto} minutos`
+  if(hora >= 0 && hora < 12){
+    img.src='assets/manha.jpg'
+    document.body.style.background = '#E3CFA5'
+  }else if(hora >=12 && hora <=18){
+    img.src='assets/tarde.jpg'
+    document.body.style.background = '#E39074'
+  }else{
+    img.src='assets/noite.jpg'
+    document.body.style.background = '#44324C'
+  }
+}
