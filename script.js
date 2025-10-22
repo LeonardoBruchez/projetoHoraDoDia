448e8969fa304ff36716eea98fae3b42bd217cf6
LeonardoBruchez <bruchezleonardo@gmail.com>
2025-10-21 22:06:55 -0300

    commit adjust color copyLink in footer

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function Load(){
   var msg = document.querySelector('p#msg')
   var img = document.querySelector('img#imagem')
+  var copyLink = document.querySelector('a#copyLink')
   var now = new Date()
-  var hora = now.getHours()
+  var hora = 1
   var minuto = now.getMinutes()
   msg.textContent = `Agora são ${hora} horas e ${minuto} minutos`
   if(hora >= 0 && hora < 12){
+    copyLink.style.color = 'gray'
     img.src='assets/manha.jpg'
     document.body.style.background = '#E3CFA5'
   }else if(hora >=12 && hora <=18){
